badf92cc5ffa04cc92b342f9cffb5426058e4fff
Ravishankar <61575048+ravishankar270@users.noreply.github.com>
2024-02-18 22:58:24 +0530

    Update DetailApi.js

--- src/DetailApi.js
+++ src/DetailApi.js
@@ -10,14 +10,13 @@ export function Detail(props){
     const value={list,setList}
     useEffect(()=>{
         const options = {
-        method: 'GET',
-        url: 'https://google-books.p.rapidapi.com/volumes',
-        params: {key: 'AIzaSyBAaiX80typIvWxnW5fahg_JqoGVNdEdww'},
-        headers: {
-            'x-rapidapi-key': '850365e9e7msh2c418e549d0ae29p1e26e3jsnec3114e6999d',
-            'x-rapidapi-host': 'google-books.p.rapidapi.com'
-        }
-        };
+            method: 'GET',
+            url: 'https://books-api7.p.rapidapi.com/books/get/random/',
+            headers: {
+              'X-RapidAPI-Key': 'ae6fff33acmsh6d2c16ed65df292p19c0e7jsn4fe73144140d',
+              'X-RapidAPI-Host': 'books-api7.p.rapidapi.com'
+            }
+          };
 
         axios.request(options).then(function (response) {
             setList(response.data.items)
